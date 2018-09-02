e237ed9c8c5d164caf2382768f327ff553864361
dehasi <rgaleyev@gmail.com>
2018-09-02 02:52:12 +0200

    SF-1 Create an annotation

---
+++ src/main/java/me/summerframework/beans/factory/Component.java
@@ -0,0 +1,9 @@
+package me.summerframework.beans.factory;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/** Created by Ravil on 02/09/2018. */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Component {
+}
