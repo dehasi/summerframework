458c90628250e90c5d15e3f31e99e8db9acb636d
dehasi <rgaleyev@gmail.com>
2018-09-03 20:07:33 +0200

    SF-2 Add annotation autowired

---
+++ src/main/java/me/summerframework/beans/factory/annotation/Autowired.java
@@ -0,0 +1,9 @@
+package me.summerframework.beans.factory.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/** Created by Ravil on 03/09/2018. */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Autowired {
+}
