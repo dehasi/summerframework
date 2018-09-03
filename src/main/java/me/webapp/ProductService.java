eba3ec3657af915079518a96fc64b29e8831a57d
dehasi <rgaleyev@gmail.com>
2018-09-03 20:09:22 +0200

    SF-2 use autowired

--- src/main/java/me/webapp/ProductService.java
+++ src/main/java/me/webapp/ProductService.java
@@ -1,7 +1,6 @@
 package me.webapp;
 
 import me.summerframework.beans.factory.annotation.Autowired;
-import me.summerframework.beans.factory.stereotype.Component;
 import me.summerframework.beans.factory.stereotype.Service;
 
 /** Created by Ravil on 02/09/2018. */
